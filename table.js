d1040304d631558a7e66a2547593cd3c0b87c2c8
johnsmalley <john.m.smalley@gmail.com>
2016-12-04 14:12:31 -0500

    refactor table code to have table in it's own file

---
+++ table.js
@@ -0,0 +1,57 @@
+if(!d3.chart) d3.chart = {};
+
+d3.chart.table = function() {
+  var data;
+  var width;
+
+  function chart(container) {
+    console.log("container: ", container)
+    console.log("data in table.js", data)
+    console.log("width", width)
+
+    var table = container.append("table")
+
+    var rows = table.selectAll("tr.row")
+      .data(data)
+
+    var rowsEnter = rows.enter()
+      .append("tr").classed("row", true)
+
+    rowsEnter.append("td")
+      .text(function(d) { return d.data.score })
+
+    rowsEnter.append("td")
+      .append("a")
+      .attr("href", function(d) { return d.data.url })
+      .append("img")
+      .attr("src", function(d) { return d.data.thumbnail })
+
+    rowsEnter.append("td")
+      .append("a")
+      .attr("href", function(d) { return d.data.url })
+      .text(function(d) { return d.data.title })
+
+    rowsEnter.append("td")
+      .text(function(d) { return d.data.ups })
+
+    rowsEnter.append("td")
+      .text(function(d) { return d.data.downs })
+
+    rows.exit().remove();
+
+  }
+
+
+  chart.data = function(value) {
+    if (!arguments.length) return data;
+    data = value;
+    return chart;
+  }
+
+  chart.width = function(value) {
+    if (!arguments.length) return width;
+    width = value;
+    return chart;
+  }
+  return chart;
+}
